9d635bd43ed0614f7b198818e35d44d77cd8a468
Fabian Emmanuel <emmanuel.fabian.dev@gmail.com>
2022-01-28 00:32:00 +0100

    FE for ethereum blockchain thumpsUp portal

---
+++ src/components/Loader.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Loader.css';
+
+const Loader = () => (
+  <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+);
+
+export default Loader;
